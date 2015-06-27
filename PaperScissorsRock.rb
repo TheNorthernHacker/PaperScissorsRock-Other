7eb5b1b2be2f947b1922c5f1aac4da20cb3edad8
tedda42 <tedda42@gmail.com>
2015-06-27 15:53:48 +1000

    Ruby formatting

--- PaperScissorsRock.rb
+++ PaperScissorsRock.rb
@@ -1,41 +1,41 @@
-puts "Do you choose rock, paper or scissors?"
+puts 'Do you choose rock, paper or scissors?'
 userChoice = gets.chomp
 computerChoice = rand
-if (computerChoice < 0.34)
-  computerChoice = "rock";
-elsif (computerChoice <= 0.67)
-  computerChoice = "paper";
+if computerChoice < 0.34
+  computerChoice = 'rock';
+elsif computerChoice <= 0.67
+  computerChoice = 'paper';
 else
-  computerChoice = "scissors";
+  computerChoice = 'scissors';
 end
-puts "User    : " + userChoice
-puts "Computer: " + computerChoice
+puts 'User    : ' + userChoice
+puts 'Computer: ' + computerChoice
 
 def compare(choice1, choice2)
-  if (choice1 === choice2)
-    return "The result is a tie!"
-  elsif (choice1 === "rock")
-    if (choice2 === "scissors")
-      return "rock wins!"
+  if choice1 === choice2
+    return 'The result is a tie!'
+  elsif choice1 === 'rock'
+    if choice2 === 'scissors'
+      return 'rock wins!'
     else
-      return "paper wins!"
+      return 'paper wins!'
     end
-  elsif (choice1 === "paper")
-    if (choice2 === "rock")
-      return "paper wins!"
+  elsif choice1 === 'paper'
+    if choice2 === 'rock'
+      return 'paper wins!'
     else
-      return "scissors wins!"
+      return 'scissors wins!'
     end
-  elsif (choice1 === "scissors")
-    if (choice2 === "rock")
-      return "rock wins!"
+  elsif choice1 === 'scissors'
+    if choice2 === 'rock'
+      return 'rock wins!'
     else
-      return "scissors wins!"
+      return 'scissors wins!'
     end
   else
-    return "Invalid choice '" + choice1 +"'";
+    return 'Invalid choice '' + choice1 +''';
   end
 end
 
-result= compare userChoice, computerChoice
+result = compare userChoice, computerChoice
 puts result
