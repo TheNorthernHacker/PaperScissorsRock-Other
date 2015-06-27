ee884e13dd681604b9914f52a0a7fe34ef84465c
tedda42 <tedda42@gmail.com>
2015-06-27 15:59:46 +1000

    Python: Add computerChoice

--- PaperScissorsRock.py
+++ PaperScissorsRock.py
@@ -1,5 +1,14 @@
 from sys import stdin
+from random import random
 
 print "Do you choose rock, paper or scissors?"
-userChoice = stdin.readline()
+userChoice = stdin.readline().rstrip()
+computerChoice = random()
+if computerChoice < 0.34:
+    computerChoice = "rock";
+elif computerChoice <= 0.67:
+    computerChoice = "paper";
+else:
+    computerChoice = "scissors";
 print "User    : " + userChoice
+print "Computer: " + computerChoice
