86e880047ac05deb87e6f09c93f2012559677835
tedda42 <tedda42@gmail.com>
2015-06-27 14:58:10 +1000

    Add computer choice

--- PaperScissorsRock.rb
+++ PaperScissorsRock.rb
@@ -1,4 +1,12 @@
 puts "Do you choose rock, paper or scissors?"
 userChoice = gets
 puts "User    : " + userChoice
-# puts "Computer: " + computerChoice
+computerChoice = rand
+if (computerChoice < 0.34)
+    computerChoice = "rock";
+elseif (computerChoice <= 0.67)
+    computerChoice = "paper";
+else
+    computerChoice = "scissors";
+end
+puts "Computer: " + computerChoice
