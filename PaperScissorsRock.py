2f09525cb37c94eac5fbf275a430b92932dcd98b
tedda42 <tedda42@gmail.com>
2015-06-27 16:15:33 +1000

    Python: Add compare function implementation

--- PaperScissorsRock.py
+++ PaperScissorsRock.py
@@ -13,8 +13,28 @@ else:
 print "User    : " + userChoice
 print "Computer: " + computerChoice
 
+
 def compare(choice1, choice2):
-    return "TODO"
+    if choice1 == choice2:
+        return 'The result is a tie!'
+    elif choice1 == 'rock':
+        if choice2 == 'scissors':
+            return 'rock wins!'
+        else:
+            return 'paper wins!'
+    elif choice1 == 'paper':
+        if choice2 == 'rock':
+            return 'paper wins!'
+        else:
+            return 'scissors wins!'
+    elif choice1 == 'scissors':
+        if choice2 == 'rock':
+            return 'rock wins!'
+        else:
+            return 'scissors wins!'
+    else:
+        return 'Invalid choice '' + choice1 +''';
+
 
 result = compare(userChoice, computerChoice);
 print result
